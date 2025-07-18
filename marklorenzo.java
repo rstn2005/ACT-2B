c13beba2a47092600c07cc40962d4d3ca0c8e552
CL1-PC09 <CL1-PC09@DESKTOP-63APOED>
2025-07-18 09:16:02 +0800

    sample program ACT-2B

---
+++ marklorenzo.java
@@ -0,0 +1,12 @@
+public class marklorenzo {
+    public static void main(String [] args) {
+    
+    String pangalan = "marklorenzo";
+    int edad = 18;
+    char favoritLetter = 'S';
+    
+    System.out.println (pangalan);
+    System.out.println (edad);
+    System.out.println (favoritLetter);
+    } 
+}
---
+++ src/main/java/com/mycompany/carlrstn/Carlrstn.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.carlrstn;
+
+/**
+ *
+ * @author CL1-PC09
+ */
+public class Carlrstn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
