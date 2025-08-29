6069ed244fc010b5fbb9b2a706633ab674788643
CL1-PC08 <CL1-PC08@DESKTOP-LQ2DKJM.lan>
2025-08-29 10:28:51 +0800

    new activity

--- src/main/java/com/mycompany/carlrstn/Computation.java
+++ src/main/java/com/mycompany/carlrstn/Computation.java
@@ -11,21 +11,57 @@ import java.util.Scanner;
 public class Computation {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("[1] - Addition");
+        System.out.println("[2] - Subtraction");
+        System.out.println("[3] - Multiplication");
+        System.out.println("[4] - Division");
         
-        System.out.println("Enter a number: ");
+        System.out.println("Enter your choice: ");
         int x = input.nextInt();
-        System.out.println("Input another number: ");
-        int y = input.nextInt();
         
-        int sum = x + y;
-        int diff = x - y;
-        int prod = x * y;
-        int quo = x / y;
-        
-        System.out.println("The sum of 2 number is " + sum);
-        System.out.println("The Difference of 2 number is " + diff);
-        System.out.println("The Product of 2 number is " + prod);
-        System.out.println("The Quotient of 2 number is " + quo);
-        
+        if(x==1)
+        {
+               System.out.println("Enter a number: ");
+               int num1 = input.nextInt();
+               System.out.println("Enter another number: ");
+               int num2 = input.nextInt();
+               
+               int sum = num1 + num2;
+               System.out.print("The Sum of 2 number is: " + sum);
+        }
+        else if(x==2)
+        {
+               System.out.println("Enter a number: ");
+               int num1 = input.nextInt();
+               System.out.println("Enter another number: ");
+               int num2 = input.nextInt();
+               
+               int sub = num1 - num2;
+               System.out.print("The Sum of 2 number is: " + sub);
+        }
+        else if(x==3)
+        {
+               System.out.println("Enter a number: ");
+               int num1 = input.nextInt();
+               System.out.println("Enter another number: ");
+               int num2 = input.nextInt();
+               
+               int mul = num1 * num2;
+               System.out.print("The Sum of 2 number is: " + mul);
+        }
+        else if(x==4)
+        {
+               System.out.println("Enter a number: ");
+               int num1 = input.nextInt();
+               System.out.println("Enter another number: ");
+               int num2 = input.nextInt();
+               
+               int div = num1 / num2;
+               System.out.print("The Sum of 2 number is: " + div);
+        }
+        else
+        {
+            System.out.println("INVALID INPUT");
+        }
     }
 }
